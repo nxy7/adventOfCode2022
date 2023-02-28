545c0ab323947b6b0bd1e4d9e1dacd184eaf151d
nxyt <lolnoxy@gmail.com>
2023-02-28 18:03:41 +0100

    flake

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "A very basic flake";
+
+  outputs = { self, nixpkgs }: rec {
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+
+    packages.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [ zig nodePackages_latest.ts-node ];
+    };
+
+  };
+
+}
